6ca44f903ab2781b417129b9c86d2a597843ec33
ChengCalvin <calvincheng_95@hotmail.com>
2020-07-20 18:16:32 -0400

    Update Metric check box item spacing

--- webpagedesign/src/components/MetricCheckBox/MetricCheckBox.js
+++ webpagedesign/src/components/MetricCheckBox/MetricCheckBox.js
@@ -10,11 +10,15 @@ export default (props) => {
   return (
     <div>
       <div className="Box__Item">
-        <input className="Checkbox__Style" type="checkbox" id="mycheck" />
-        <label className="Label__Styling">{props.children}</label>
-        <button className="Dropdown__Item" onClick={toggleDescriptionHandler}>
-          V
-        </button>
+        <div className="Checkbox__Style">
+          <input type="checkbox" id="mycheck" />
+        </div>
+        <div className="Label__Dropdown-item__Spacing">
+          <label className="Label__Styling">{props.children}</label>
+          <button className="Dropdown__Item" onClick={toggleDescriptionHandler}>
+            V
+          </button>
+        </div>
       </div>
       {open ? (
         <div className="Description__Box">{props.value}</div>
