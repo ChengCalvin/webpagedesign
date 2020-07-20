d705a46544f436223798e041adc76ba5c219dc3b
ChengCalvin <calvincheng_95@hotmail.com>
2020-07-20 17:45:46 -0400

    Update Metric Description Box to display below checkbox item and update backdrop background opacity

--- webpagedesign/src/components/MetricCheckBox/MetricCheckBox.js
+++ webpagedesign/src/components/MetricCheckBox/MetricCheckBox.js
@@ -4,14 +4,23 @@ import "./MetricCheckBox.css";
 
 export default (props) => {
   const [open, setOpen] = useState(false);
+  const toggleDescriptionHandler = () => {
+    setOpen((open) => !open);
+  };
   return (
-    <div className="Box__Item">
-      <input type="checkbox" id="mycheck" />
-      <label className="Label__Styling">{props.children}</label>
-      <button className="Dropdown__Item" onClick={() => setOpen(!open)}>
-        V
-      </button>
-      {open ? props.value : <p></p>}
+    <div>
+      <div className="Box__Item">
+        <input className="Checkbox__Style" type="checkbox" id="mycheck" />
+        <label className="Label__Styling">{props.children}</label>
+        <button className="Dropdown__Item" onClick={toggleDescriptionHandler}>
+          V
+        </button>
+      </div>
+      {open ? (
+        <div className="Description__Box">{props.value}</div>
+      ) : (
+        <div></div>
+      )}
     </div>
   );
 };
