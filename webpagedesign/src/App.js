96e0c623fbdac9ea18b6821d4e29118603f8e075
Calvin <calvincheng_95@hotmail.com>
2020-07-28 18:29:33 -0400

    Add QuestionDisplay to remove repeating components (MetricBox, IncomeStatementBox, CashFlowBox)

--- webpagedesign/src/App.js
+++ webpagedesign/src/App.js
@@ -1,12 +1,10 @@
 import React from "react";
 
 import Toolbar from "./components/Toolbar/Toolbar";
-import MetricBox from "./components/MetricBox/MetricBox";
-import IncomeStatementBox from "./components/IncomeStatementBox/IncomeStatementBox";
-import CashFlowBox from "./components/CashFlowBox/CashFlowBox";
 import Toolbarbot from "./components/Toolbar/Toolbarbot/Toolbarbot";
 import Scrollupbutton from "./components/Scrollupbutton/Scrollupbutton";
 import Progressbar from './components/Progressbar/Progressbar';
+import QuestionDisplay from './components/QuestionsDisplay/QuestionDisplay'
 
 function App() {
 
@@ -17,48 +15,14 @@ function App() {
       <main
         style={{
           marginTop: "25px",
+          marginBottom: "25px",
           display: "flex",
           justifyContent: "center",
           alignItems: "center",
         }}
       >
-        <MetricBox />
+        <QuestionDisplay />
       </main>
-      <div
-        style={{
-          width: "85%",
-          borderBottom: "1px solid gray",
-          marginLeft: "5%",
-          marginTop: "5rem",
-        }}
-      ></div>
-      <div
-        style={{
-          display: "flex",
-          justifyContent: "center",
-          marginTop: "50px",
-        }}
-      >
-        <IncomeStatementBox />
-      </div>
-      <div
-        style={{
-          width: "85%",
-          borderBottom: "1px solid gray",
-          marginLeft: "5%",
-          marginTop: "5rem",
-        }}
-      ></div>
-      <div
-        style={{
-          display: "flex",
-          justifyContent: "center",
-          marginTop: "50px",
-          marginBottom: "90px",
-        }}
-      >
-        <CashFlowBox />
-      </div>
 
         <Scrollupbutton />
 
---
+++ webpagedesign/src/components/QuestionsDisplay/QuestionDisplay.js
@@ -0,0 +1,73 @@
+import React from 'react';
+
+import MetricCheckBox from "../MetricCheckBox/MetricCheckBox";
+
+import './QuestionDisplay.css'
+
+export default () => {
+    const questions = [{
+        Question : 'What should you be looking at?',
+        Question_Description : "Check these metrics to stay on top of your financial health",
+        metric : [
+        { Label: "Revenue", Description: "123" },
+        { Label: "Cost of Goods Sold (COGS)", Description: "blah blah blah" },
+        {
+          Label:
+            "EBITDA (Earnings before Interest, Tax, Deprecitation & Amortization)",
+          Description: "wow ebidta?",
+        },
+        { Label: "Net Income", Description: "blah blah blah" },
+        { Label: "Abnormal Costs", Description: "blah blah blah" },
+        {
+          Label: "Days Receivables (aka Days Sales Outstanding)",
+          Description: "blah blah blah",
+        },
+        { Label: "Inventory", Description: "blah blah blah" },
+        {
+          Label: "Days Inventory Outstanding (DIO)",
+          Description: "blah blah blah",
+        },
+        { Label: "Days Payable Outstanding (DPO)", Description: "blah blah blah" },
+        { Label: "Quick Ratio", Description: "blah blah blah" },
+        {
+          Label: "Debt to Total Assets (Debt Ratio)",
+          Description: "blah blah blah",
+        },
+        { Label: "Gross Margin", Description: "blah blah blah" },
+        { Label: "Net Profit Margin", Description: "blah blah blah" },]
+    },{
+        Question : 'What is an Income Statment?',
+        Question_Description : "Understanding your revenues and expenses you can figure out where you are doing well and where you can improve",
+        metric : [{Label: "Fill out Income Statement", Description: "Here is the form to fill"}]
+    },{
+        Question : 'What is Cash Flow Statment?',
+        Question_Description : "The cash flow statement tells you how much money is entering and leaving your business",
+        metric : [{Label: "Fill out Cash Flow Statement", Description: "Here is the form to fill"}]
+    }]
+
+
+    return( <div className="Box-Display">
+        {questions.map(question => (
+            <div key={question.Question}>
+                <div>
+                    <h1>{question.Question}</h1>
+                    <p>{question.Question_Description}</p>
+                </div>
+                <div>
+                    { question.metric.map(metric => {
+                        return <MetricCheckBox value={metric.Description} key={metric.Label}>{metric.Label}</MetricCheckBox>
+                    })}
+                </div>
+                <div
+                    style={{
+                    width: "85%",
+                    borderBottom: "1px solid gray",
+                    marginLeft: "5%",
+                    marginTop: "5rem",
+                    }}
+                ></div>
+            </div>
+            ))
+        }
+    </div>)
+}
